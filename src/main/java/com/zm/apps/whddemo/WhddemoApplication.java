c6ac435ddd868e4d4ac438be64f55030c74019ff
Administrator <Administrator@windows10.microdone.cn>
2019-05-05 22:40:47 +0800

    第一次提交项目代码

---
+++ src/main/java/com/zm/apps/whddemo/WhddemoApplication.java
@@ -0,0 +1,17 @@
+package com.zm.apps.whddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SpringBoot 我的项目学习分支2
+ * @author Administrator
+ *
+ */
+@SpringBootApplication
+public class WhddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhddemoApplication.class, args);
+	}
+}
